259e092dac8d5ef12119f5bbfa49d4b6a75246b8
Zane0 <zlzohvzew@hotmail.com>
2016-10-12 17:44:22 +0800

    增加按几率返回 true 的方法

---
+++ ZLib/ZLib/Util/Sampling.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ZLib.Util
+{
+	public class Sampling
+	{
+		/// <summary>
+		/// 按几率返回 true，如果 100 则 100% 返回 true，0 则 100% 返回 false
+		/// </summary>
+		/// <param name="percent"></param>
+		/// <returns></returns>
+		public bool GetTrue(int percent)
+		{
+			Random _ra = new Random();
+			int _r = _ra.Next(100);
+			return percent > _r;
+		}
+	}
+}
