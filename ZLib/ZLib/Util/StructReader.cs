2730756c33c6aad1565882f9464e32b0aaa46c0e
Zane0 <zlzohvzew@hotmail.com>
2016-12-22 16:51:42 +0800

    增加在定长的流中读取结构数组的功能，在 ZLib.Util.StructReader 类中增加 ReadStructArrayFromStream 方法

--- ZLib/ZLib/Util/StructReader.cs
+++ ZLib/ZLib/Util/StructReader.cs
@@ -20,5 +20,25 @@ namespace ZLib.Util
 			return SerializeHelper.Bytes2Struct<T>(buffer);
 #line default
 		}
+
+		/// <summary>
+		/// 从流中读取结构数组
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="stream"></param>
+		/// <param name="len">流的长度</param>
+		/// <returns></returns>
+		public static T[] ReadStructArrayFromStream<T>(Stream stream, int len) where T : struct
+		{
+#line hidden
+			int _size = len / Marshal.SizeOf(typeof(T));
+			T[] _ts = new T[_size];
+			for (int _i = 0; _i <= _size - 1; _i++)
+			{
+				_ts[_i] = StructReader.ReadStructFromStream<T>(stream);
+			}
+			return _ts;
+#line default
+		}
 	}
 }
