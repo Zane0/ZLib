8273a47a95783979c030265d19989f91ece1d9ec
Zane0 <zlzohvzew@hotmail.com>
2018-08-09 11:23:18 +0800

    增加用于计算执行方法花费时间的方法

---
+++ ZLib/ZLib/Util/PerformanceHelper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ZLib.Util
+{
+	public static class Performance
+	{
+		/// <summary>
+		/// 获取方法执行花费的时间
+		///	TimeSpan _ts = CheckTime(() =>
+		///	{
+		///	    for (int _i = 0; _i < 100000000; _i++)
+		///	    {
+		///	        int _j = (int)123456789L;
+		///	    }
+		///	});
+		///	Console.WriteLine("执行方法共花费{0}毫秒", _ts.TotalMilliseconds);
+		/// </summary>
+		/// <param name="act"></param>
+		/// <returns></returns>
+		public static TimeSpan CheckTime(Action act)
+		{
+			Stopwatch _st = Stopwatch.StartNew();
+			act();
+			return _st.Elapsed;
+		}
+	}
+}
